833c0ca6c4c9c0aef13dc4cf6a276d3d95487817
Geraldo Pires <geraldopireslacerda771@gmail.com>
2022-12-11 15:18:38 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1;
+do {
+    //bloco
+
+    console.log(`Passo ${c}`);
+    c++; // c = c + 1;
+} while (c <= 6);
+
+/*
+var c = 1;
+while (c <= 6) {
+    //bloco
+    
+    console.log(`Passo ${c}`);
+    c++; // c = c + 1;
+}
+*/
+
+/*
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
