ac393c6442424120d25f9980d17040e2939e7cea
Geraldo Pires <geraldopireslacerda771@gmail.com>
2022-12-14 22:20:32 -0300

    Validação do Exercício 16 - OK

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,26 +1,38 @@
 function contar() {
     var inicio = document.getElementById('inicio').value;
     var fim = document.getElementById('fim').value;
-    var passo = Number(document.getElementById('passo').value);
+    var passo = document.getElementById('passo').value;
 
     var resultado = document.getElementById('resultado');
     resultado.innerHTML = 'contando:<br>';
 
     if (inicio == '') {
         alert('[ERRO] - Entre com um valor de Inicio');
+        return 0;
     } else {
-        alert('inicio valido: ' + Number(inicio));
+        inicio = Number(inicio);
     }
     if (fim == '' || fim <= 0) {
         alert('[ERRO] - Entre com um valor de Fim');
+        return 0;
     } else {
-        alert('Fim valido: ' + Number(fim));
+        fim = Number(fim);
+    }
+    if (passo == '') {
+        alert('[ERRO] - Entre com um valor de Passo');
+        return 0;
+    } else {
+        passo = Number(passo);
+        if (passo <= 0) {
+            alert('PASSO igual a Zero, Considerando PASSO 1');
+            passo = 1;
+        }
+
+        for (inicio; inicio <= fim; inicio += passo) {
+            resultado.innerHTML += `${inicio} &#x1F449`;
+        }
+        resultado.innerHTML += '&#x1F3F4';
     }
     /*
-    for (inicio; inicio <= fim; inicio += passo) {
-        resultado.innerHTML += `${inicio} &#x1F449`;
-        console.log(inicio);
-    }*/
-
-    resultado.innerHTML += '&#x1F3F4';
+     */
 }
