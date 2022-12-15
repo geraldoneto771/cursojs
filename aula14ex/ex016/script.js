52ad64aa8bbf5369debc340347fd865754522db5
Geraldo Pires <geraldopireslacerda771@gmail.com>
2022-12-14 21:30:10 -0300

    Implementando Validações

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,14 +1,26 @@
 function contar() {
-    var inicio = Number(document.getElementById('inicio').value);
-    var fim = Number(document.getElementById('fim').value);
+    var inicio = document.getElementById('inicio').value;
+    var fim = document.getElementById('fim').value;
     var passo = Number(document.getElementById('passo').value);
 
     var resultado = document.getElementById('resultado');
     resultado.innerHTML = 'contando:<br>';
+
+    if (inicio == '') {
+        alert('[ERRO] - Entre com um valor de Inicio');
+    } else {
+        alert('inicio valido: ' + Number(inicio));
+    }
+    if (fim == '' || fim <= 0) {
+        alert('[ERRO] - Entre com um valor de Fim');
+    } else {
+        alert('Fim valido: ' + Number(fim));
+    }
+    /*
     for (inicio; inicio <= fim; inicio += passo) {
         resultado.innerHTML += `${inicio} &#x1F449`;
         console.log(inicio);
-    }
+    }*/
 
     resultado.innerHTML += '&#x1F3F4';
 }
